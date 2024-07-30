90c314b32650c6f30f9e00844b00dec4de485fcb
Devarsh <spidercoder007@gmail.com>
2024-07-30 11:04:01 -0400

    small changes

--- course/api/app.py
+++ course/api/app.py
@@ -25,6 +25,16 @@ from dotenv import load_dotenv
 from langchain.chains.query_constructor.base import AttributeInfo
 from langchain.retrievers.self_query.base import SelfQueryRetriever
 # from langchain_community import query
+
+
+from langchain.chains import create_history_aware_retriever
+from langchain_core.messages import HumanMessage, AIMessage
+
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
 import time
 import pickle
 
@@ -94,29 +104,84 @@ retriever = SelfQueryRetriever.from_llm(
 )
 
 
-prompt = ChatPromptTemplate.from_template(
-    """
-    Answer the questions based on the provided context only.
-    Please provide a good response based on the question and context
-    <context>
-    {context}
-    </context>
-    Questions:{question}
-
-    """
+### Contextualize question ###
+contextualize_q_system_prompt = """Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
 )
 
-document_chain = create_stuff_documents_chain(llm,prompt)
-# retriever = db3.as_retriever()
-retriever_chain = create_retrieval_chain(retriever,document_chain)
+
+### Answer question ###
+qa_system_prompt = """You are an assistant for question-answering tasks. \
+Use the following pieces of retrieved context to answer the question. \
+If you don't know the answer, just say that you don't know. \
+Use three sentences maximum and keep the answer concise.\
+
+{context}"""
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", qa_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+### Statefully manage chat history ###
+store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
+
+# # Streamed response emulator
+# def response_generator():
+#     response = conversational_rag_chain.invoke(
+#     {"input": RunnablePassthrough()},
+#     config={
+#         "configurable": {"session_id": "abc123"}
+#     },  # constructs a key "abc123" in `store`.
+#     )["answer"]
+#     for word in response.split():
+#         yield word + " "
+#         time.sleep(0.01)
+
 try:
-    entry_point_chain=RunnableParallel({"context":retriever,"question":RunnablePassthrough()})
+    entry_point_chain=RunnableParallel({"context":conversational_rag_chain.invoke(
+    {"input":RunnablePassthrough()},
+    config={
+        "configurable": {"session_id": "abc123"}
+    },  # constructs a key "abc123" in `store`.
+    )["answer"]})
 except:
     entry_point_chain="No Context"
 
+
 add_routes(
     app,
-    entry_point_chain|prompt|llm|StrOutputParser(),
+    entry_point_chain,
     path="/chat"
 )
 
